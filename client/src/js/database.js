c301ba3b96b8b9b98e87b28cb793e93856e8613e
Benoit <fbenoit2077@gmail.com>
2024-03-26 17:04:17 -0400

    installed idb and added function store and retrieve content indexedDB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,30 @@
 import { openDB } from 'idb';
 
-const initdb = async () =>
-  openDB('jate', 1, {
+
+const initDB = async () => {
+  return openDB('jate', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
-        return;
+      if (!db.objectStoreNames.contains('jate')) {
+        const store = db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+        
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
     },
   });
+};
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Function to store content in IndexedDB
+export const putDb = async (content) => {
+  const db = await initDB();
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  await store.put({ content });
+  await tx.done;
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-
-initdb();
+// Function to retrieve content from IndexedDB
+export const getDb = async () => {
+  const db = await initDB();
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  return store.getAll();
+};
