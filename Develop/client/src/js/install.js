b60e3bb3e516d01b1c90bccf173bc88a008849d7
Benoit <fbenoit2077@gmail.com>
2024-03-26 01:53:26 -0400

    logic for install.js

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -1,11 +1,32 @@
-const butInstall = document.getElementById('buttonInstall');
+let deferredPrompt;
 
-// Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+  
+  event.preventDefault();
+ 
+  deferredPrompt = event;
+ 
+  butInstall.style.display = 'block';
+});
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+
+butInstall.addEventListener('click', async () => {
+ 
+  butInstall.style.display = 'none';
+  
+  deferredPrompt.prompt();
+  
+  const { outcome } = await deferredPrompt.userChoice;
+  
+  if (outcome === 'accepted') {
+    console.log('PWA installation accepted by user');
+  } else {
+    console.log('PWA installation canceled by user');
+  }
+});
+
+
+window.addEventListener('appinstalled', (event) => {
+  console.log('PWA installed successfully');
+});
