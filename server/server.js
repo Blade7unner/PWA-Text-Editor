6b4cc91f4be6263cbbf2c83d9ece70dd54be0607
Benoit <fbenoit2077@gmail.com>
2024-03-27 01:43:18 -0400

    last commit

--- server/server.js
+++ server/server.js
@@ -4,26 +4,26 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Serve static files from the 'client' directory
+
 app.use(express.static(path.join(__dirname, '../client')));
 
-// Serve manifest.json from the root URL
+
 app.get('/manifest.json', (req, res) => {
   res.sendFile(path.join(__dirname, '../manifest.json'));
 });
 
-// Serve icons from the '/assets/icons' directory
+
 app.get('/assets/icons/:iconName', (req, res) => {
   const iconName = req.params.iconName;
   res.sendFile(path.join(__dirname, `../client/src/images/logo.png`));
 });
 
-// Serve other routes
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/index.html'));
 });
 
-// Start the server
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
